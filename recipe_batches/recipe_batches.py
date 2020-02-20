81ec5e75ea9a8bfecf0c54af4b1e5e374fc34645
Victor <arowovici@gmail.com>
2020-02-20 08:32:44 +0100

    Completed Recipe Algo

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,29 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    batches = 0
+    difference = 999999
+    smallest_recipe = next(iter(recipe))
+    if len(recipe) > len(ingredients):
+        return 0
+    for i in recipe:
+        if recipe[i] > ingredients[i]:
+            return 0
+        if ingredients[i] - recipe[i] < difference:
+            difference = ingredients[i] - recipe[i]
+            smallest_recipe = i
+            batches = 1
+
+    batches += difference//recipe[smallest_recipe]
+    return batches
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
